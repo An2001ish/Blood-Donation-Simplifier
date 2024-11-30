586889d2854c45900e0f1010f2f29a9eae6c1600
Anish Baraily <An2001.baraily@gmail.com>
2024-11-30 10:06:52 +0545

    node server done

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const cors = require('cors');
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+//mongodb connection
+connectDB();
 
 const app = express()
 app.use(express.json())
@@ -12,9 +16,12 @@ app.use(cors())
 app.use(morgan('dev'))
 
 app.use('/api/v1/test',require('./routes/testRoutes'));
+app.use('/api/v1/auth',require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 3900;
 
+
 app.listen(PORT, ()=>{
-    console.log(`Node Server Running in ${process.env.DEV_MODE} mode on PORT ${process.env.PORT}`);
+    console.log(`Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+      .bgBlue.white);
 })
